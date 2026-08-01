51692be1a87503feb49e04f45c1b438c79e2637f
arrozqueimado99999 <122797107+arrozqueimado99999@users.noreply.github.com>
2026-07-31 23:59:00 -0300

    first commit

---
+++ trade-mkt/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBEDZkHK-1WZNLKqUfTo3h2vRwMjzGkcro",
+  authDomain: "trade-mkt-f04db.firebaseapp.com",
+  projectId: "trade-mkt-f04db",
+  storageBucket: "trade-mkt-f04db.firebasestorage.app",
+  messagingSenderId: "794991765802",
+  appId: "1:794991765802:web:9ef2c8715aa8e887889653"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
